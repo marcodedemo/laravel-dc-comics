03de44837e4536e00561af55cbb8408c95113dce
marcodedemo <marco.dedemo@gmail.com>
2023-05-17 13:26:23 +0200

    feat: bonus

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -32,13 +32,45 @@
         </div>
 
         <div id="button-container" class="text-center">
-            <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
 
-                @csrf
-                @method('DELETE')
-                <button type="submit" id="delete" class="mb-5">Elimina Fumetto</button>
-            </form>
         </div>
+
+
+        <!-- Button trigger modal -->
+        <div id="button-container" class="mb-5">
+            <button type="button" id="delete" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Elimina Fumetto</button>
+        </div>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+
+            <div class="modal-dialog modal-dialog-centered">
+
+            <div class="modal-content">
+                <div class="modal-header">
+                
+                <h2 class="modal-title fs-5" id="staticBackdropLabel">Attenzione!</h2>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">Sei sicuro di voler eliminare il fumetto?</div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+
+                <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Elimina</button>
+                </form>
+
+                </div>
+            </div>
+
+            </div>
+
+        </div>
+  
+          
         
         <div id="button-container" class="text-center">
             <a href="{{route('comics.index')}}" ><button class="mb-5">Torna alla lista fumetti</button></a>
